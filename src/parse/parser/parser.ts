a7cd573776b407dac5ffa88c155c37a059aac360
Enoch Pang <enoch@enochpang.com>
2026-04-30 17:52:29 -0400

    Combine parser and events into one file

--- src/parse/parser/parser.ts
+++ src/parse/parser/parser.ts
@@ -7,9 +7,9 @@ import {
 	OTokenKind,
 	type SyntaxKind,
 } from "../syntax/syntax_kind.ts";
-import { buildTree, type ParseError, type ParseEvent } from "./events.ts";
 import { parseDeclaration } from "./grammar.ts";
 import type { LexResult } from "../lexer/lexer.ts";
+import { GreenBuilder } from "../syntax/green_builder.ts";
 
 export type MarkOpened = {
 	index: number;
@@ -18,6 +18,48 @@ export type MarkOpened = {
 export type MarkClosed = {
 	index: number;
 	kind: NodeKind;
+};
+
+export type StartNode = {
+	tag: "START_NODE";
+	kind: NodeKind;
+	forward_parent: number | undefined;
+};
+
+export type FinishNode = {
+	tag: "FINISH_NODE";
+};
+
+export type AddToken = {
+	tag: "ADD_TOKEN";
+};
+
+export type SkipToken = {
+	tag: "SKIP_TOKEN";
+};
+
+export type ErrorEvent = {
+	tag: "ERROR";
+	message: string;
+};
+
+export type PlaceholderEvent = {
+	tag: "PLACEHOLDER";
+};
+
+export type ParseEvent =
+	| StartNode
+	| FinishNode
+	| AddToken
+	| SkipToken
+	| PlaceholderEvent
+	| ErrorEvent;
+
+export type EventKind = ParseEvent["tag"];
+
+export type ParseError = {
+	offset: number;
+	message: string;
 };
 
 export type ParseResult<T> = {
@@ -35,6 +77,85 @@ export function parse(lex_result: LexResult): ParseResult<GreenNode> {
 		tree: tree,
 		errors: errors,
 	};
+}
+
+export function buildTree(
+	lex_result: LexResult,
+	events: ParseEvent[],
+): [GreenNode, ParseError[]] {
+	const tokens = lex_result.tokens;
+	const builder: GreenBuilder = new GreenBuilder();
+	const errors: ParseError[] = [];
+	const kinds: SyntaxKind[] = [];
+	let offset = 0;
+	let cursor = 0;
+
+	// Remove the last close event event so the stack has one item after iterating through the events.
+	const last_event = events.pop();
+	assert.notEqual(last_event, undefined);
+	assert.equal(last_event?.tag, "FINISH_NODE");
+
+	for (let event_index = 0; event_index < events.length; event_index++) {
+		const event = events[event_index];
+		events[event_index] = { tag: "PLACEHOLDER" };
+
+		switch (event.tag) {
+			case "START_NODE": {
+				kinds.push(event.kind);
+				let idx = event_index;
+				let ofp = event.forward_parent;
+
+				while (ofp) {
+					idx += ofp;
+
+					const event = events[idx];
+					events[idx] = { tag: "PLACEHOLDER" };
+
+					if (event.tag === "START_NODE") {
+						kinds.push(event.kind);
+						ofp = event.forward_parent;
+					} else {
+						assert.fail("Unreachable");
+					}
+				}
+
+				for (let i = kinds.length - 1; i >= 0; i--) {
+					const kind = kinds[i];
+					builder.startNode(kind);
+				}
+
+				kinds.length = 0;
+
+				break;
+			}
+			case "ADD_TOKEN": {
+				const token = tokens[cursor];
+				builder.addToken(token.kind, token.text);
+				offset += token.getLength();
+				cursor += 1;
+				break;
+			}
+			case "FINISH_NODE": {
+				builder.finishNode();
+				break;
+			}
+			case "SKIP_TOKEN": {
+				const token = tokens[cursor];
+				offset += token.getLength();
+				cursor += 1;
+				break;
+			}
+			case "ERROR": {
+				errors.push({
+					offset: offset,
+					message: event.message,
+				});
+				break;
+			}
+		}
+	}
+
+	return [builder.getTree() as GreenNode, errors];
 }
 
 export class Parser {
